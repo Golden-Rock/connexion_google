6fee3bdefc6bfff9c4b39056a294d44c7c57fdb6
Daryl <borismbakop611@gmail.com>
2025-03-22 10:52:55 +0100

    update constant google

--- config/google.php
+++ config/google.php
@@ -3,6 +3,7 @@
  * Configuration pour l'API Google OAuth
  */
 
-define('GOOGLE_CLIENT_ID', '2096648057-r8vtjhmmherqhlcstpsv7bpcc65dnvbk.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URI', 'http://localhost:3000/config/callback.php');
+define('GOOGLE_CLIENT_ID', '93966177149-cou6ijtqon89mghh8hcq66pfoi3d6qb4.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', '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');
+define('GOOGLE_REDIRECT_URI', 'http://localhost/auth-google-php/config/callback.php');
+/* define('GOOGLE_REDIRECT_URI', 'http://localhost:3000/config/callback.php'); */
